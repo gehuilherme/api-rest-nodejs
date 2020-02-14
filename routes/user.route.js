01e7c12312ed3dddca3327ba22faed3781a9ee29
Guilherme Henrique <32115850+gehuilherme@users.noreply.github.com>
2020-02-13 23:30:04 -0300

    Add files via upload

---
+++ routes/user.route.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+
+// Express route
+const userExpressRoute = express.Router();
+
+// User schema
+let UserSchema = require('../model/user.model');
+
+// Get users
+userExpressRoute.route('/').get((req, res) => {
+    UserSchema.find((error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// Create user
+userExpressRoute.route('/create-user').post((req, res, next) => {
+    UserSchema.create(req.body, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+// Get single user
+userExpressRoute.route('/get-user/:id').get((req, res) => {
+    UserSchema.findById(req.params.id, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+// Update user
+userExpressRoute.route('/update-user/:id').put((req, res, next) => {
+    UserSchema.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data)
+            console.log('User successfully updated!')
+        }
+    })
+})
+
+// Delete user
+userExpressRoute.route('/remove-user/:id').delete((req, res, next) => {
+    UserSchema.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+        }
+    })
+})
+
+module.exports = userExpressRoute;
